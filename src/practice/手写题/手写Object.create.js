2520af820b526f1fd361a9b1ed700bd2714dbf31
yuusooi <176738229+yuusooi@users.noreply.github.com>
2026-02-26 15:40:45 +0800

    chore: clean up demos for public release

--- src/practice/手写题/手写Object.create.js
+++
@@ -1,8 +0,0 @@
-function create(proto) {
-  if (typeof proto !== 'object' && typeof proto !== 'function') {
-    throw new TypeError('Object prototype may only be an Object or null')
-  }
-  function F() {} //创建临时构造函数
-  F.prototype = proto //将原型指向传入对象
-  return new F() //返回new的实例
-}
